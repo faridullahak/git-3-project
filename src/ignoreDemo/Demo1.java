ce10cbc0969f2d7a76b7224c8c76ff106d210744
faridullahak <faridullah.akbari@tekschool.us>
2023-08-18 17:10:19 -0400

    ignored unnessery filse

---
+++ src/ignoreDemo/Demo1.java
@@ -0,0 +1,10 @@
+package ignoreDemo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is practice for ingoneing unnecessery files");
+	}
+
+}
